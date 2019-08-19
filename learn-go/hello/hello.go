9883a1a79fc4acf52c16027304edca2da5b61814
my-blog.github.io <wxd963996380@gmail.com>
2019-08-19 20:14:17 +0800

    finally refacotr hello extract func

--- learn-go/hello/hello.go
+++ learn-go/hello/hello.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-const helloPrefix = "hello, "    //常量可以提高程序的性能
+const englishHelloPrefix = "hello, "    //常量可以提高程序的性能
 const spanish = "Spanish"
 const french = "French"
 const spanishHelloPrefix = "Hola, "
@@ -13,21 +13,20 @@ func hello(name string, language string) string{
 		name = "world"
 	}
 
-	prefix := helloPrefix
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	if language == spanish {
-		return prefix + name
-	}
-
-	if language == french {
-		return prefix + name
-	}
-	return helloPrefix + name
+	return 
 }
 
 func main(){
