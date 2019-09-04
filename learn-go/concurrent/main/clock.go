9084581d6bb778eedaa2cf73b905f6a53c8964e2
my-blog.github.io <wxd963996380@gmail.com>
2019-09-04 22:19:31 +0800

    concurrent

---
+++ learn-go/concurrent/main/clock.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+//顺序执行的时钟服务器
+
+func main(){
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn)  {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
---
+++ learn-go/concurrent/main/spinner.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+// spinner 和 fib(n) 两个函数会同时执行
+func main() {
+	go spinner(100 * time.Millisecond)
+	const n = 45
+	fibN := fib(n)
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) {
+	for{
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
