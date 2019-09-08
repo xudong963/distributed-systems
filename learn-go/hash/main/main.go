b5269e792f073866984883c1011c2bcac4d16cf8
my-blog.github.io <wxd963996380@gmail.com>
2019-09-08 11:31:11 +0800

    hash

---
+++ learn-go/hash/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"hash/fnv"
+)
+
+func hash(s string) uint32 {
+	h := fnv.New32a()
+	a, b := h.Write([]byte(s))
+	fmt.Println(a)   // a is the length of s
+	fmt.Println(b)   // b is err, usually is nil
+	return h.Sum32()
+}
+
+func main() {
+	fmt.Println(hash("HelloWorld"))
+	fmt.Println(hash("HelloWorld."))
+}
