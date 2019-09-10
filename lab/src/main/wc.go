1899dec00a87580b7a961c7ece7dedd70e06fd86
my-blog.github.io <wxd963996380@gmail.com>
2019-09-10 18:55:02 +0800

    finish wc.go

--- lab/src/main/wc.go
+++ lab/src/main/wc.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"mapreduce"
 	"os"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 //
@@ -15,6 +18,14 @@ import (
 //
 func mapF(filename string, contents string) []mapreduce.KeyValue {
 	// Your code here (Part II).
+	words := strings.FieldsFunc(contents, func(r rune) bool {
+		return !unicode.IsLetter(r)
+	})
+	var kv []mapreduce.KeyValue
+	for _, word := range words {
+		kv = append(kv, mapreduce.KeyValue{word, ""} )
+	}
+	return kv
 }
 
 //
@@ -24,6 +35,8 @@ func mapF(filename string, contents string) []mapreduce.KeyValue {
 //
 func reduceF(key string, values []string) string {
 	// Your code here (Part II).
+
+	return strconv.Itoa(len(values))
 }
 
 // Can be run in 3 ways:
