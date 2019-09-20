25c893de0d34de7302ae99547852351440f27d4a
DreaMer963 <wxd963996380@gmail.com>
2019-09-20 11:41:40 +0800

    finish lab2 Part A GetState

--- lab/src/raft/raft.go
+++ lab/src/raft/raft.go
@@ -71,7 +71,7 @@ type Raft struct {
 	// volatile state on leaders(reinitialized after elected)
 	nextIndex[] int               // for each server, index of the next log entry to send to that server(initialized to leader last log index+1)
 	matchIndex[] int              // for each server, index of highest log entry known to be replicated on server (initialized to 0)
-	
+
 }
 
 // return currentTerm and whether this server
@@ -81,6 +81,8 @@ func (rf *Raft) GetState() (int, bool) {
 	var term int
 	var isleader bool
 	// Your code here (2A).
+	term = rf.currentTerm
+	isleader = rf.state==Leader
 	return term, isleader
 }
 
